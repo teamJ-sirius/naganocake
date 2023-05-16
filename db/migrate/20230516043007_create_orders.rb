7e2e1a7297121f5073ff1c901a565c991716218f
senpai-kouhai <129678369+senpai-kouhai@users.noreply.github.com>
2023-05-16 06:28:09 +0000

    Add ordertable orderdetailtable

--- db/migrate/20230516043007_create_orders.rb
+++ db/migrate/20230516043007_create_orders.rb
@@ -1,7 +1,14 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
-
+      t.integer :customer_id #customersテーブルの顧客ID
+      t.integer :postage #一律800円
+      t.integer :total_price #cart_price + 800円
+      t.integer :order_status #(0:入金待ち 1:入金確認 2:製作中 3:発送準備中 4:発送済み)のいずれか デフォルト値は0
+      t.string :post_code
+      t.string :address
+      t.string :name
+      t.integer :payment_method #(0:クレジットカード 1:銀行振込)のいずれか
       t.timestamps
     end
   end
