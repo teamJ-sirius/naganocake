3b182755a02b72c0700241db1b4f848d10b58e8f
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-05-17 01:20:24 +0000

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
+
+  #会員側のルーティング設定
+  get 'items' => 'public/items#index'
+  #管理者側のルーティング設定
+  namespace :admin do
+    get 'items' => 'admin/items#index'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
