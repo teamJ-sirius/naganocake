5b71e76e3dd4d6e06efbc4aeef31577c4fe85c76
yuukiyuukiyuuki <128203554+yuukiyuukiyuuki@users.noreply.github.com>
2023-05-18 01:20:36 +0000

    itemのindexの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   namespace :public do
     root to: 'homes#top'
     get '/about' => 'homes#about'
-    
+
   # 配送先
     resources :shipping_addresses, only: [:index, :create, :edit, :update, :destroy]
-    
+
   # 注文
     resources :orders, only: [:new, :index, :show, :create, :confirm, :complete]
-    
+
   # カート
     resources :cart_items, only: [:index, :update, :destroy, :create]do
       delete 'destroy_all' => 'cart_items#destroy_all'
@@ -16,17 +16,17 @@ Rails.application.routes.draw do
 
   # カスタマー
     resources :customers, only: [:show, :edit, :update]do
-      menber do
+      member do
         get :unsubscribe
         patch :withdraw
       end
     end
-    
+
   # 商品
     resources :itemes, only: [:index, :show]
-    
+
   end
-  
+
   devise_for :admins
   devise_for :customers
 
