c2524c2d67a37772b8550e7c73a62ca0fb776c4a
EC2 Default User <ec2-user@ip-172-31-1-102.ap-northeast-1.compute.internal>
2023-05-19 08:08:58 +0000

    test

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,8 +1,17 @@
 class Public::OrdersController < ApplicationController
   def new
+    cart_items = current_customer.cart_items
+    if cart_items.present?
+      @order = Order.new
+      @shipping_addresses = ShippingAddress.all
+    else
+      flash[:notice] = "・カートが空です"
+    end
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
 　 
   end
 
