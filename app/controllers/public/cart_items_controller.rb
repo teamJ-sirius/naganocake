bf381286bb3820f38868fda0d8691771f6f7671a
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-05-19 00:12:04 +0000

    cart_item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -11,10 +11,42 @@ class Public::CartItemsController < ApplicationController
     redirect_to cart_item_path
   end
 
+  def increase
+    @cart_item.increment!(:quantity, 1)
+    redirect_to request.referer, notice: 'Successfully update your cart'
+  end
+
+  def decrease
+    decrease_or_destroy(@cart_item)
+    redirect_to request.referer, notice: 'Successfully update your cart'
+  end
+
+  def destroy
+    @cart_item.destroy
+    redirect_to request.referer, notice: 'Successfully deleted one cart item'
+  end
+
   private
 
   def set_cart_item
     @cart_item = current_customer.cart_items.find(params[:id])
+  end
+
+  def increase_or_create(item_id)
+    cart_item = current_customer.cart_items.find_by(item_id:)
+    if cart_item
+      cart_item.increment!(:quantity, 1)
+    else
+      current_customer.cart_items.build(item_id:).save
+    end
+  end
+
+  def decrease_or_destroy(cart_item)
+    if cart_item.quantity > 1
+      cart_item.decrement!(:quantity, 1)
+    else
+      cart_item.destroy
+    end
   end
 
   def cart_item_params
