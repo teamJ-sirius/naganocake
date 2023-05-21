dc0488926c0d15825c1a3e03f17b491676d492de
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-05-21 05:54:53 +0000

    修正

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,12 @@
 class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
+
+  validates :item_id, :quantity, presence: true
+  validates :quantity, numericality:{ only_integer: true }
+
+  def sum_of_price
+    item.taxin_price * quantity
+  end
+
 end
