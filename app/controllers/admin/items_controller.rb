eeb2aa590ac0f3e08f782acf85e16e4dac9428aa
yuukiyuukiyuuki <128203554+yuukiyuukiyuuki@users.noreply.github.com>
2023-05-22 14:23:26 +0000

    hoshizumi

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -9,8 +9,11 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save
+    if@item.save
     redirect_to admin_items_path
+    else
+      render :new
+    end
   end
 
   def show
