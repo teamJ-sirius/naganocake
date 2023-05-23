9aca2bc4fb70051a62c2e4c97407eafdf77a1efe
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-05-23 06:41:57 +0000

    header

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,6 +2,7 @@ class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
 
   def index
+    @order = Order.all
     @q = Order.ransack(params[:q])
     @orders = @q.result(distinct: true)
   end
