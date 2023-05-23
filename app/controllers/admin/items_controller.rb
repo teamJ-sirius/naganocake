57329927af6f0b388707851e57d9b759d60c7ca1
EC2 Default User <ec2-user@ip-172-31-1-102.ap-northeast-1.compute.internal>
2023-05-23 01:31:51 +0000

    order

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -9,8 +9,11 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save
-    redirect_to admin_items_path
+    if @item.save
+     redirect_to admin_items_path
+    else
+     render :new
+    end
   end
 
   def show
