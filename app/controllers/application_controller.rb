67e2f5a1dfff6066ccda98fed572ec1738814d6f
senpai-kouhai <129678369+senpai-kouhai@users.noreply.github.com>
2023-05-24 02:45:08 +0000

    add change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
-  
+
+  before_action :authenticate_admin!, if: :admin_url
+
+
+
+ def admin_url
+
+  request.fullpath.include?("/admin")
+
+ end
+
 end
